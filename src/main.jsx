5b0717272c775813bbe31b9fbec2bb5ba423b3e1
l3lackMegas ༼ つ ◕_◕ ༽つ <contact@jaruwat.dev>
2023-04-23 21:37:20 +0700

    feat: add css folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './css/index.css'
+import './css/home.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
