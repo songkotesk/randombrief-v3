3b6a9536084a60ebf9feb9739711cd2f897df513
songkotesk <49570248+songkotesk@users.noreply.github.com>
2023-04-23 21:10:24 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./component/Navbar"
+import Hero from "./component/Hero"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center border-b-2'>
+      <div className='flex'>
+        <p className='text-xl font-semibold p-4 pt-3 pl-10'>สบ.</p>
+        <p className='text-base font-light p-4'>สนุกกับการออกแบบ</p>
+      </div>
+      <ul className='flex text-base font-light'>
+        <li className='p-4 pr-10'>บล็อก</li>
+        <li className='p-4 pr-10'>เกี่ยวกับ</li>
+        <li className='p-4 pr-10 fa-lg'><FontAwesomeIcon icon={faSun}/></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
